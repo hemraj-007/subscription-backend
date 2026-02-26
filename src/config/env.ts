c5ae0cb7f63a977b95a7263ccf34157b3453bbd7
hemraj-007 <140829250+hemraj-007@users.noreply.github.com>
2026-02-27 00:27:34 +0530

    changes in env

--- src/config/env.ts
+++ src/config/env.ts
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const env = {
-  PORT: process.env.PORT || 5000,
+  PORT: process.env.PORT || 3001,
   DATABASE_URL: process.env.DATABASE_URL as string,
   JWT_SECRET: process.env.JWT_SECRET as string,
   JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN || "7d",
